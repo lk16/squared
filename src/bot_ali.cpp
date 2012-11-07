5256f4a32d90e4f934ea911ff2d4115c574325cc
Luuk Verweij <luuk_verweij@msn.com>
2012-11-07 15:03:05 +0100

    updated bot_ali heuristic to be compatible with different board sizes than 8x8

--- src/bot_ali.cpp
+++ src/bot_ali.cpp
@@ -8,49 +8,37 @@ bot_ali::bot_ali(color _c, int _max_depth, int _max_endgame_depth):
 int bot_ali::heuristic(const board* b)
 {
   int res,i;
+  const static int disc_count_factor = 2;
+  const static int corner_factor = 30;
+  const static int xsquare_factor = -4;
+  
+  const static int corners_x[4] = { 0, 0, (FIELD_SIZE-1), (FIELD_SIZE-1) };
+  const static int corners_y[4] = { 0, (FIELD_SIZE-1), 0, (FIELD_SIZE-1) };
+  
+  const static int xsquares_x[4] = { 1, 1, (FIELD_SIZE-2), (FIELD_SIZE-2) };
+  const static int xsquares_y[4] = { 1, (FIELD_SIZE-2), 1, (FIELD_SIZE-2) };
   
   res = 0;
   
-  const static int disc_count_factor = 2;
-
-  if(FIELD_SIZE==8){
-  
-    const static int location_value[10] = {
-      30,-1,-1,-1,-4,0,1,1,1,2
-    };
-    
-  
-    
-    
-    const static int location[64] = 
-    {
-      0,1,2,3,3,2,1,0,
-      1,4,5,6,6,5,4,1,
-      2,5,7,8,8,7,6,5,
-      3,6,8,9,9,8,6,3,
-      3,6,8,9,9,8,6,3,
-      2,5,7,8,8,7,6,5,
-      1,4,5,6,6,5,4,1,
-      0,1,2,3,3,2,1,0
-    };
-    
-    
-    for(i=0;i<64;i++){
-      if(b->discs[c].to_ulong() & (1ul << i)){
-        res += location_value[location[i]];
-      }
-      else if(b->discs[opponent(c)].to_ulong() & (1ul << i)){
-        res -= location_value[location[i]];
-      } 
+  for(i=0;i<4;i++){
+    if(b->has_color(corners_x[i],corners_y[i],c)){
+      res += corner_factor;
+    }
+    else if(b->has_color(corners_x[i],corners_y[i],opponent(c))){
+      res -= corner_factor;
+    }
+    if(b->has_color(xsquares_x[i],xsquares_y[i],c)){
+      res += xsquare_factor;
+    }
+    else if(b->has_color(xsquares_x[i],xsquares_y[i],opponent(c))){
+      res -= xsquare_factor;
     }
     
   }
-  
+    
   res -= disc_count_factor * b->count_discs(c);
   res += disc_count_factor * b->count_discs(opponent(c));
   
-
-
   return res;
 }
 
