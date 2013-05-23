060c0f13fae649eaf497d6980f420cd5ca5f73bc
Luuk Verweij <luuk_verweij@msn.com>
2013-05-23 16:48:22 +0200

    added missing files

--- src/bots/bot_ali.cpp
+++ src/bots/bot_ali.cpp
@@ -29,14 +29,19 @@ void bot_ali::evaluate_depth_level(const board* boards, int* heurs, int count, i
   
   for(int id=count-1;id>=0;--id){
     heurs[id] = -alpha_beta(boards + id,alpha,MAX_HEURISTIC,depth);
-    alpha = max(alpha,heurs[id]);
+    std::cout << heurs[id] << std::endl;
+    if(heurs[id] > alpha){
+      alpha = heurs[id];
+    }
+    std::cout << '.' << std::flush;
+    
   }
 }
 
 
 void bot_ali::do_move(const board* b,board* res) 
 {
-  int alpha,child_count,best_move_id;
+  int alpha,child_count,best_move_id,depth;
   board children[TOTAL_FIELDS/2];  
   
   nodes = 0;
@@ -57,9 +62,12 @@ void bot_ali::do_move(const board* b,board* res)
     return;
   } 
  
+  depth = max_depth;
+ 
   if(TOTAL_FIELDS - (b->discs[BLACK] | b->discs[WHITE]).count() <= (unsigned)max_endgame_depth){
     //TODO separate endgame method
     // return something;
+    depth = max_endgame_depth;
   }
   
   
@@ -72,10 +80,14 @@ void bot_ali::do_move(const board* b,board* res)
   
   for(int d=1;d<=max_depth;d++){
     memcpy(&big_child_stack,children,child_count*sizeof(board));
+#if SQUARED_BOT_ENABLE_OUTPUT
+    std::cout << "Depth " << d << ": ";
+    std::cout.flush();
+#endif 
     evaluate_depth_level(big_child_stack,heurs,child_count,d);
     sort_boards(children,heurs,child_count);
 #if SQUARED_BOT_ENABLE_OUTPUT
-    std::cout << "Depth " << d << ": best heur = " << heurs[child_count-1] << std::endl;
+    std::cout << " " << heurs[child_count-1] << std::endl;
 #endif  
   }
 
@@ -161,7 +173,7 @@ int bot_ali::heuristic(const board* b)
   //                              0   1   2   3   4   5   6   7   8   9
   static int open_loc_val[10] = { 50, -8, -7, -6,-10, -3, -3, -4, -3, -2 };
   static int  mid_loc_val[10] = { 40, -4, -3, -2, -6, -4, -2, -2, -2,  0 };
-  static int  end_loc_val[10] = { 30,  5,  7,  9, -2,  1,  0,  3,  1,  5 };
+  static int  end_loc_val[10] = { 20,  5,  7,  9, -2,  1,  0,  7,  1,  5 };
   
   int disc_count = (b->discs[WHITE] | b->discs[BLACK]).count();
   
---
+++ src/main/debug.cpp
@@ -0,0 +1,84 @@
+#include "bots/bot_ali.hpp"
+#include "game/board.hpp"
+#include "game/util.hpp"
+
+#include <ctime>
+#include <iostream>
+#include <algorithm>
+#include <sys/time.h>
+
+int get_descendants(board* b,int max_depth){
+  
+  int read_layer_size = 1;
+  int write_layer_size = 0; 
+  int res = 0;
+  
+  int read_offset = 1;
+  int write_offset = read_offset + read_layer_size;
+  
+  for(int d=0;d<max_depth;d++){
+    for(int i=0;i<read_layer_size;i++){
+      int children;
+      (b+read_offset+i)->get_children(b+write_offset+write_layer_size,&children);
+      write_layer_size += children;
+    }
+    res += write_layer_size;
+    read_offset = write_offset;
+    read_layer_size = write_layer_size;
+    write_offset += write_layer_size;
+    write_layer_size = 0; 
+  }
+  return res;
+}
+
+void do_random_moves(board* b,int n){
+  board m[32];
+  int count;
+  for(int i=n-1;i--;){
+    b->get_children(m,&count);
+    if(count == 0){
+      return;
+    }
+    *b = m[rand()%count];
+  }
+}
+
+
+
+int main(int argc,char **argv){
+  int counter=0,i;
+  timeval start,end;
+  board in[40],out;
+  
+  
+  if(argc < 2){
+    std::cout << "not enough args\n";
+    return 0;
+  }
+  
+  
+  int depth = fromstr<int>(std::string(argv[1]));
+  bot_ali ali(WHITE,depth,depth);
+  
+  in[0].reset();
+  for(i=1;i<40;i++){
+    in[i] = in[i-1];
+    do_random_moves(in+i,1);
+  }
+  
+  gettimeofday(&start,NULL);
+  for(i=1;i<40;i++){
+    for(int j=0;j<1;j++){
+      ali.do_move(in+i,&out);
+      counter += ali.nodes;
+    }
+  }
+  gettimeofday(&end,NULL);
+
+  double diff = (end.tv_sec + (end.tv_usec / 1000000.0)) - (start.tv_sec + (start.tv_usec / 1000000.0));
+  
+  std::cout << "Result at depth " << depth << "\t\t";
+  std::cout << counter << " / " << diff << " = " << (counter/diff) << " nodes per second.\n";
+
+  return 0;
+}
