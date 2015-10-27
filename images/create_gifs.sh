b8735e3396d647f7f9453a2c81aa2bb4f41e7b5b
Luuk Verweij <luuk_verweij@msn.com>
2015-10-27 21:36:11 +0100

    made create_gifs.sh way faster by making it asynchronous

--- images/create_gifs.sh
+++ images/create_gifs.sh
@@ -6,6 +6,6 @@ do
     do
         out_file="${lhs%.*}_to_${rhs%.*}.gif"
         echo "Generating $out_file"
-        convert $lhs $rhs -loop 1 -morph 3 $out_file
+        convert $lhs $rhs -loop 1 -morph 8 $out_file &
     done
 done
